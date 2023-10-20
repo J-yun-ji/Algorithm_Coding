6e1dd03215d98f883772df22ae99e2a47ed5155c
yunji <66745172+J-yun-ji@users.noreply.github.com>
2023-10-21 01:00:09 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>  
+#include<string.h>
+
+
+int main(void) {
+
+    char str[100];
+    scanf("%s", str);
+
+    printf("%d", strlen(str));
+    
+    return 0;
+}
