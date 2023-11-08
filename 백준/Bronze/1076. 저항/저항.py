389cf617bf09cf05c472579d5e968d86d3e4d4a7
yunji <66745172+J-yun-ji@users.noreply.github.com>
2023-11-09 00:30:00 +0900

    [Bronze II] Title: 저항, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.py
+++ 백준/Bronze/1076. 저항/저항.py
@@ -1,7 +1,11 @@
-color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+firstCol = input()
+secondCol = input()
+thirdCol = input()
+Color = {'black' : 0, 'brown' : 1, 'red' : 2, 'orange' : 3,
+'yellow' : 4, 'green' : 5, 'blue' : 6, 'violet' : 7, 'grey' : 8, 'white' : 9}
+j = 1
+num3 = 1
+num2 = 0
+num1 = 0
 
-a = str(color.index(input()))
-b = str(color.index(input()))
-c = str(10**color.index(input()))
-
-print(int(a+b+c[1:]))
+print((10 * Color[firstCol] +  Color[secondCol]) * (10 ** Color[thirdCol]))
