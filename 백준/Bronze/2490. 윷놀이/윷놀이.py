e5d3d32669ae08a052c2e21fe377e2c42520b6de
yunji <66745172+J-yun-ji@users.noreply.github.com>
2023-12-05 23:43:14 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,12 +1,12 @@
-for i in range(3):
-    a = list(map(int, input().split()))
-    if a.count(0) == 1:
+for i in range(3) :
+    game = list(map(int, input().split()))
+    if game.count(0) == 1 :
         print("A")
-    elif a.count(0) == 2:
+    elif game.count(0) == 2 :
         print("B")
-    elif a.count(0) == 3:
+    elif game.count(0) == 3 :
         print("C")   
-    elif a.count(0) == 4:
+    elif game.count(0) == 4 :
         print("D")
-    else:
-        print("E") 
+    else :
+        print("E")
