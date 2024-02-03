6c8d6efd29f580f0c51809a5e4527ab174513fbc
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-02-04 00:06:04 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,11 +1,16 @@
-n = int(input())
+C = int(input())
+# 1. 평균값을 구한다.
+# 2. 평균값을 넘는 학생의 개수를 구한다.
+# 3. 10에서 나누기 전체 학생 수
+# 4. 3번에서 곱하기 2번. 
 
-for _ in range(n):
-    nums = list(map(int, input().split()))
-    avg = sum(nums[1:])/nums[0]   
-    cnt = 0
-    for score in nums[1:]:
-        if score > avg:
-            cnt += 1   
-    rate = cnt/nums[0] *100
-    print(f'{rate:.3f}%')
+for _ in range(C) : 
+    score = list(map(int, input().split()))
+    avg = sum(score[1:]) / score[0]
+    student = 0
+  
+    for num in score[1:] :
+        if num > avg :
+            student += 1
+    result = student / score[0] * 100
+    print(f'{result: .3f}%') 
