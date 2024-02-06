7cce8ed6189c5b9d5ed17cedfdd2b874487a9d31
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-02-06 13:24:48 +0900

    [Silver V] Title: 셀프 넘버, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -4,22 +4,21 @@ for i in range(10000) :       # 1-10,000 값 넣기
     number.append(i)
 
 for i in range(len(number)) :
-  if i < 10 :
-    seq = i + i
-    number.remove(seq)
-  elif i < 100 :
-    seq = i + ((i - (i % 10)) / 10) + (i % 10)
-    if seq in number :
-        number.remove(seq)
-  elif i < 1000 :
-    seq = i + ((i - (i % 100)) / 100) + (((i % 100) - (i % 10)) / 10) + (i % 10)
-    if seq in number :
-        number.remove(seq)
-  elif i < 10000 :
-    seq = i + ((i - (i % 1000)) / 1000) + (((i % 1000) - (i % 100)) / 100) + (((i % 100) - (i % 10)) / 10) + (i % 10)
-    if seq in number :
-        number.remove(seq)
+    num = str(i)
+    if i < 10 :
+        number.remove(int(num[0]) + int(num[0]))
+    elif i < 100 :
+        seq = i + int(num[0]) + int(num[1])
+        if seq in number :
+            number.remove(seq)
+    elif i < 1000 :
+        seq = i + int(num[0]) + int(num[1]) + int(num[2])
+        if seq in number :
+            number.remove(seq)
+    elif i < 10000 :
+        seq = i + int(num[0]) + int(num[1]) + int(num[2]) + int(num[3])
+        if seq in number :
+            number.remove(seq)
 
-
-for i in range(len(number)) : # 셀프 넘버 출력
+for i in range(len(number)) :
     print(number[i])
