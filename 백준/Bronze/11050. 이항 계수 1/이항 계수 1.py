8873ad0b683230d1645f0f1968f3083fb57fd583
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-03-03 11:00:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -4,13 +4,10 @@ ptk = 1
 ptm = 1
 
 for i in range(N, 1, -1) :
-    ptn *= i
-    k = i - K
+    ptn *= i 
     if i <= K :
         ptk *= i
     if i <= N - K :
         ptm *= i
-    
 
 print(ptn // (ptk * ptm))
-
