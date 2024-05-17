6cc02588b63e98e69435e9f22cd1503bb00ce1f5
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-05-18 01:30:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,44 +1,39 @@
 N = int(input())
 
 house = [list(map(int, input())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
-
-newHouse = [[0] * N for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+visited = [[False] * N for _ in range(N)]
+cnt = []
 
-def dfs(x, y, k) :
+def dfs(x, y) :
 
-    if x < 0 or x >= N or y < 0 or y >= N :
+    if x < 0 or x >= N or y < 0 or y >= N:
         return 0
 
-    if house[x][y] == 1 and visited[x][y] == False :
+    if house[x][y] == 1 and visited[x][y] == False:
         visited[x][y] = True
-        newHouse[x][y] = k
-        houseCnt = 1
+        n = 1
         for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
-            houseCnt += dfs(nx, ny, k)
-        return houseCnt
+            n += dfs(nx, ny)
+        return n
     return 0
 
 
-cnt = 0
-houseCnt = []
-k = 1
-
+all = 0
 for i in range(N) :
     for j in range(N) :
-        if house[i][j] == 1 and not visited[i][j]:
-            house_size = dfs(i, j, k)
+        if house[i][j] == 1 and visited[i][j] == False : # 방문하지 않았다면.
+            house_size = dfs(i, j)
             if house_size > 0 :
-                houseCnt.append(house_size)
-                k += 1
-                cnt += 1
+                cnt.append(house_size)
+            all += 1
 
-houseCnt.sort()
-print(cnt) 
-print('\n'.join(map(str, houseCnt)))
+print(all)
+cnt.sort()
+print('\n'.join(map(str, cnt)))
+
